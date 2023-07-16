51fd5f4834828e1d5c62d5aa1488381df449cb1e
Naman Yadav <raymannky2001@gmail.com>
2023-07-16 23:06:59 +0530

    added pizzas in menu and add order now button too

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
 
 // Raw data
 const pizzaData = [
   {
-    name: "Focaccia",
+    name: "Pizza Chilliy",
     ingredients: "Bread with italian olive oil and rosemary",
     price: 6,
     photoName: "pizzas/focaccia.jpg",
@@ -39,8 +40,8 @@ const pizzaData = [
     soldOut: true,
   },
   {
-    name: "Pizza Prosciutto",
-    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    name: "Pizza Paneer",
+    ingredients: "Tomato, mozarella,Panner, ham, aragula, and burrata cheese",
     price: 18,
     photoName: "pizzas/prosciutto.jpg",
     soldOut: false,
@@ -50,43 +51,95 @@ const pizzaData = [
 // components of website we gonna build
 
 function Header() {
-  return <h1>Fast React Pizza Co.</h1>;
-}
-
-function Menu() {
   return (
-    <>
-      <h1>Menu</h1>
-      <Pizza />
-    </>
-  );
-}
-
-function Footer() {
-  const todaysDate = new Date().getHours();
-  console.log(todaysDate);
-  return (
-    <footer>
-      {new Date().toLocaleTimeString()} , We are Currently Working
-    </footer>
+    <header className="header">
+      <h1>Fast React Pizza Co.</h1>
+    </header>
   );
 }
 
 // pizaa component
-function Pizza() {
+function Pizza(props) {
+  console.log(props);
+  const sold = props.pizzaData.soldOut;
+
   return (
-    <div>
-      <img src="pizzas/spinaci.jpg" alt="pizza spinnaci" />
-      <h1>Pizza Prosciutto</h1>
-      <p>Tomato, mozarella, ham, aragula, and burrata cheese</p>
+    <li className={sold ? "pizza sold-out" : "pizza "}>
+      <img src={props.pizzaData.photoName} alt={props.pizzaData.name} />
+      <article>
+        <h3>{props.pizzaData.name}</h3>
+        <p>{props.pizzaData.ingredients}</p>
+        <span>{props.pizzaData.price}</span>
+        {sold ? <span>Sold Out</span> : null}
+      </article>
+    </li>
+  );
+}
+function Menu() {
+  const pizzas = pizzaData;
+  const pizzaCount = pizzas.length;
+  return (
+    <main className="menu">
+      <h2>Menu</h2>
+      {pizzaCount > 0 ? (
+        <ul className="pizzas">
+          {pizzaData.map((pizza) => (
+            <Pizza pizzaData={pizza} key={pizza.name} />
+          ))}
+        </ul>
+      ) : null}
+
+      {/* <Pizza
+        pizzaName="Pizza Prosciutto"
+        ingredients="Tomato, mozarella, ham, aragula, and burrata cheese"
+        pizzaPhoto="pizzas/spinaci.jpg"
+        price={10}
+      />
+      <Pizza
+        pizzaName="Pizza Margherita"
+        ingredients="Tomato and mozarella"
+        pizzaPhoto="pizzas/margherita.jpg"
+        price={12}
+      /> */}
+    </main>
+  );
+}
+
+function Order(props) {
+  return (
+    <div className="order">
+      <p>
+        we are currently open until {props.closeHour}:00, come visit us or order
+        online.
+      </p>
+      <button className="btn">Order Now</button>
     </div>
+  );
+}
+
+function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 10;
+  const closeHour = 23;
+  const isOpen = hour >= openHour && hour <= closeHour;
+
+  return (
+    <footer className="footer">
+      {isOpen ? (
+        <Order closeHour={closeHour} />
+      ) : (
+        <p>
+          We happy to take your order between {openHour}:00 and {closeHour}:00.
+        </p>
+      )}
+    </footer>
   );
 }
 
 // main app component
 function App() {
   return (
-    <div>
+    <div className="container">
       <Header />
       <Menu />
       <Footer />
